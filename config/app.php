494d9cae024be8d949e549852b4db0dbd4435415
Leo24 <leo2410@yandex.ru>
2016-08-17 13:43:29 +0300

    Add configs

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        LibreEHR\FHIR\Utilities\Providers\FHIRServiceProvider::class,
 
         /*
          * Application Service Providers...
