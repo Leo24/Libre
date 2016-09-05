9e52a32cea18ccd82080eb718c35412735b41d64
Leo24 <leo2410@yandex.ru>
2016-09-05 14:49:31 +0300

    Add routes for auth

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/register', 'RegisterController@register');
+Route::post('/login', function () {
+    return redirect('oauth/token');
+});
